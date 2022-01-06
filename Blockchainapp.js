63af0fe42903d108d197d009e91d48e4881c1b98
Amar-mishra-wq <amarmishra5464@gmail.com>
2022-01-06 11:39:24 +0530

    My New Project

---
+++ Blockchainapp.js
@@ -0,0 +1,11 @@
+const Blockchain = require('./Blockchain.js');
+
+const Block = require('./block.js');
+
+const blockchain = new Blockchain();
+
+for (let i = 1; i < 10; i++) {
+    const newData = 'Block Number : ' + ' ' + i;
+    blockchain.addBlock()
+}
+console.log(blockchain);
